8ec6218359ca7802430dba98663be03c04d79ffd
enthusiastic-mouse-1767 <ebisiemen@gmail.com>
2019-01-25 10:24:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,7 +8,7 @@ def speak_to_grandma(phrase)
   
     return "NO, NOT SINCE 1938!" 
     
-  elsif phrase != "I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
   
     return "I LOVE YOU TOO PUMPKIN!"
   
