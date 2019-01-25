b49036ef43ca467bd414084a6d49309e00150f9b
enthusiastic-mouse-1767 <ebisiemen@gmail.com>
2019-01-25 10:33:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,20 +1,12 @@
 def speak_to_grandma(phrase)
-
   if phrase != phrase.upcase()
-
     return "HUH?! SPEAK UP, SONNY!" 
-
   else 
-  
     return "NO, NOT SINCE 1938!" 
-    
-      elsif phrase == "I LOVE YOU GRANDMA!"
-  
-         return "I LOVE YOU TOO PUMPKIN!"
-    
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   end 
-  
-       end 
+  end 
 
 end 
 
